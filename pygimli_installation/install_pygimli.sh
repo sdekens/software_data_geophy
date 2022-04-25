bed495409113f5e6fb13551fe955655efc12c5f1
Siméon <simeon.dekens@umons.ac.be>
2022-04-25 08:52:01 +0200

    Fixes an error in the environment creation command

--- pygimli_installation/install_pygimli.sh
+++ pygimli_installation/install_pygimli.sh
@@ -1,5 +1,8 @@
 echo 'Creating the virtual environment pg'
-conda create -n pg -c gimli -c conda-forge pygimli=1.2.4
+#conda create -n pg -c gimli -c conda-forge pygimli=1.2.4
+#conda install -c gimli pygimli
+# This seems to be the correct command line. Needs to be tested.
+conda create -n pg -c gimli pygimli
 
 echo 'Activating virtual environment'
 conda activate pg
